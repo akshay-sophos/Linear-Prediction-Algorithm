80d90e71c63824210bf93b473284db6b0460d0c6
Akshay Sreekumar <akshaysreekumar1997@gmail.com>
2017-06-26 12:00:07 +0530

    Gap wworking

--- linp.m
+++ linp.m
@@ -22,25 +22,24 @@ for(n = 1: 92 : 44008)
     e_min(1) = Rxx_sum(1)*(1-a(1+1)^2);
     g = zeros(9,25);
     %g(1,:)
-    for(x = -10 : 14)
+    for(b = -10 : 14)
         for(q = 0 : 2)
-            g(1,x+10+1) = g(1,x+10+1) + Rxx_sum(abs(q-x)+1)*a(1+q);
+            g(1,b+10+1) = g(1,b+10+1) + Rxx_sum(abs(q-b)+1)*a(1+q);
         end%g(1,k)
     end
-    stem([-10:14],g(1,:));
-    gam(2) = -g(1,10+2)/g(1,10);
+    gam(2) = -g(1,10+3)/g(1,10+1);
     e_min(2) = e_min(1).*(1-gam(1)^2);
-    
+   
     for(m = 2 : 10)
-        for(k = -10 : 14)
-            K = fliplr(lagmatrix(g(1,:),m)');
-            K(isnan(K)) = 0;
-            g(m,10+k+1) = g(m-1,10+k+1) + gam(m).*K;
-        end
+        K = fliplr(lagmatrix(g(m-1,:),m-(2*(m-2)))');
+        K(isnan(K)) = 0;
+        g(m,:) = g(m-1,:) + gam(m).*K;
         gam(m+1) = -g(m,10+m+2)/g(m,10+1);
         e_min(m+1) = e_min(m).*(1-gam(m)^2);  
     end
-    
+    if(n > 93+92+92*5)
+        plot(e_min)
+    end
 end
         
         
